7f6b3a958749a62c43b03d708b2ce6e5f94f80cc
Radouan Larabi <rlarabi@student.1337.ma>
2023-08-12 14:33:01 +0100

    display the map on the window

--- raycatsing.c
+++ raycatsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   raycatsing.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rlarabi <rlarabi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 04:21:17 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/08/12 05:17:18 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/08/12 13:07:04 by rlarabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,15 @@ void display_pixel(t_general *info)
 	void	*mlx_win;
 	int i;
 	i = 0;
+	int j;
+	j = 0;
+	int x;
+	x = 0;
+	int y;
+	y = 0;
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 45 * info->dimensions[0], 45 * info->dimensions[1], "cub3d");
+	// mlx_key_hook()
 	// while (i < 45 * info->dimensions[0])
 	// {
 	// 	if (info->valid_map[])
@@ -32,9 +39,37 @@ void display_pixel(t_general *info)
 	// 		/* code */
 	// 	}
 
-	// 	mlx_pixel_put(mlx,mlx_win,i,0, 0x00FF0000);
 	// 	i++;
 	// }
+	printf("info->dimensions[1] %d\t info->dimensions[0] %d\n", info->dimensions[1], info->dimensions[0]);
+	//exit(0);
+	while (i < 45)
+	{
+		j = 0;
+		while (j < 45)
+		{
+			y = 0;
+			while(y < info->dimensions[1])
+			{
+				x = 0;
+				while(x < info->dimensions[0])
+				{
+					printf("%d | %d\n", (i * 45) + x ,(j * 45) + y);
+					if (info->valid_map[y][x] == '1')
+						mlx_pixel_put(mlx,mlx_win,(x * 45) + j ,(y * 45) + i, 0xFFFF00);
+					if (info->valid_map[y][x] == '0')
+						mlx_pixel_put(mlx,mlx_win,(x * 45) + j ,(y * 45) + i, 0x00FFFF);
+					if (ft_strchr("NEWS", info->valid_map[y][x]))
+						mlx_pixel_put(mlx,mlx_win,(x * 45) + j ,(y * 45) + i, 0x00F00F);
+					x++;
+				}
+				y++;
+			}
+			j++;
+		}
+		i++;
+	}
+	
 	printf("|%s|\n",info->valid_map[0]);
 	printf("|%s|\n",info->valid_map[1]);
 	printf("|%s|\n",info->valid_map[2]);
