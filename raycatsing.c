c84c9f1a5ebe7e341bb80ff6d337ed8b98e6c5d1
Hassan Lakhal <hlakhal-@e1r12p1.1337.ma>
2023-08-12 22:23:00 +0100

    player

--- raycatsing.c
+++ raycatsing.c
@@ -6,7 +6,7 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 04:21:17 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/08/12 20:13:15 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/08/12 22:19:47 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,27 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
+}
+
+void display_player(int x, int y, t_data img)
+{
+	int i;
+	int j;
+
+	i = 0;
+	x *= 45;
+	y *= 45;
+	while (i < 5)
+	{
+		j = 0;
+		while (j < 5)
+		{
+			my_mlx_pixel_put(&img,x + j, y + i,0x4500FF);
+			j++;
+		}
+		i++;
+	}
+
 }
 
 void display_pixel(t_general *info)
@@ -43,8 +64,6 @@ void display_pixel(t_general *info)
 	img.img = mlx_new_image(mlx, 45 * info->dimensions[0], 45 * info->dimensions[1]);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-	printf("info->dimensions[1] %d\t info->dimensions[0] %d\n", info->dimensions[1], info->dimensions[0]);
-	//exit(0);
 	while (i < 45)
 	{
 		j = 0;
@@ -58,17 +77,14 @@ void display_pixel(t_general *info)
 				{
 					//printf("%d | %d\n", (i * 45) + x ,(j * 45) + y);
 					if (info->valid_map[y][x] == '1')
-					{
 						my_mlx_pixel_put(&img, (x * 45) + j, (y * 45) + i, 0x00FF0000);
-						mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+					if (info->valid_map[y][x] == '0')
+						my_mlx_pixel_put(&img, (x * 45) + j, (y * 45) + i, 0x00FFFFFF);
+					if (ft_strchr("NEWS", info->valid_map[y][x]))
+					{
+						display_player(x, y,img);
+						my_mlx_pixel_put(&img, (x * 45) + j, (y * 45) + i, 0x00FFFFFF);
 					}
-					// if (info->valid_map[y][x] == '0')
-					// 	mlx_pixel_put(mlx,mlx_win,(x * 45) + j ,(y * 45) + i, 0x00000F);
-					// if (ft_strchr("NEWS", info->valid_map[y][x]))
-					// {
-					// 	printf("|x:%d|\t|y:%d|\n",info->info_player->pos_x,info->info_player->pos_y);
-					// 	mlx_pixel_put(mlx,mlx_win, (info->info_player->pos_x * 20) + 1 ,(info->info_player->pos_y * 20) + 1, 0x00F00F);
-					// }
 					x++;
 				}
 				y++;
@@ -77,12 +93,6 @@ void display_pixel(t_general *info)
 		}
 		i++;
 	}
-
-	printf("|%s|\n",info->valid_map[0]);
-	printf("|%s|\n",info->valid_map[1]);
-	printf("|%s|\n",info->valid_map[2]);
-	printf("|%s|\n",info->valid_map[3]);
-	printf("|%s|\n",info->valid_map[4]);
-	printf("|%s|\n",info->valid_map[5]);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
