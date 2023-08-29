9d146abf0223cb730164d674b4906f926c0d80ea
Radouan Larabi <rlarabi@c3r3p5.1337.ma>
2023-08-29 16:24:58 +0100

    debugging distance from wall

--- raycasting/moves.c
+++ raycasting/moves.c
@@ -6,7 +6,7 @@
 /*   By: rlarabi <rlarabi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 12:59:27 by rlarabi           #+#    #+#             */
-/*   Updated: 2023/08/29 12:46:39 by rlarabi          ###   ########.fr       */
+/*   Updated: 2023/08/29 16:23:25 by rlarabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 void	slide_player(t_general *info, double pos_x, double pos_y)
 {
-	if (info->alpha != 270 && info->alpha != 90 && info->alpha != 180 && info->alpha != 0)
+	if (info->alpha != 270.00 && info->alpha != 90 && info->alpha != 180 && info->alpha != 0)
 	{
 		if (info->valid_map[(int)info->info_player->pos_y][(int)pos_x] != '1'
 			&& info->valid_map[(int)info->info_player->pos_y][(int)pos_x] != 32)
-			info->info_player->pos_x = pos_x;
+			{
+				// printf("%f\t%f\n", (pos_x * cos(((info->alpha) * PI) / 180)), pos_x);
+				info->info_player->pos_x = pos_x;
+			}
 		if (info->valid_map[(int)pos_y][(int)info->info_player->pos_x] != '1'
 			&& info->valid_map[(int)pos_y][(int)info->info_player->pos_x] != 32)
 			info->info_player->pos_y = pos_y;
@@ -37,15 +40,22 @@ void	move_up(t_general *info, t_data *img)
 		&& info->valid_map[(int)info->info_player->pos_y][(int)pos_x] == '1')
 		return ;
 	if (info->valid_map[(int)pos_y][(int)pos_x] != '1'
-		&& info->valid_map[(int)pos_y][(int)pos_x] != 32)
+		&& info->valid_map[(int)pos_y][(int)pos_x] != 32 )
 	{
+		printf("%f %f\n",fmin(info->h->lenght, info->h->lenght), fabs(30  * cos(((info->alpha - info->bita_ray) * PI)/ 180)));
+		
+		if (info->h->lenght >=  fabs(30 * cos(((info->alpha - info->bita_ray) * PI)/ 180))  && info->v->lenght >=  fabs(30 * cos(((info->alpha - info->bita_ray) * PI)/ 180))) 
+		{
+			info->info_player->pos_x = pos_x;
+			info->info_player->pos_y = pos_y;
+		}
+		else
+			slide_player(info, pos_x, pos_y);
 		// if (fmin(info->h->lenght, info->h->lenght) <= 15)
 		// {
 		// 	printf("in front of wall\n");
 		// 	return ;
 		// }
-		info->info_player->pos_x = pos_x;
-		info->info_player->pos_y = pos_y;
 	}
 	else
 		slide_player(info, pos_x, pos_y);
